d017f666133a25bd93587cc44d763429b4fc0724
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-10-26 00:32:52 +0100

    Duplicate `get` function from https://github.com/hwbrent/phrase-guessr/blob/10aa094998b1c2f44b55d5767dad8310c020b93f/src/scraper/scraper.py#L18

--- src/lib/scrape/utils.js
+++ src/lib/scrape/utils.js
@@ -1,5 +1,27 @@
+import axios from 'axios';
 
 const HOME_URL = 'https://www.omniglot.com/soundfiles/';
+
+// i just went to the url above, then looked in the network tab to see what the header
+// value was
+const USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:131.0) Gecko/20100101 Firefox/131.0';
+
+/**
+ * @summary Performs a GET request and, if successful, returns the resulting HTML as
+ * text
+ * @param {string} url - the url of the resource to fetch
+ */
+async function get(url) {
+    // make the request. specifying the header seems to reduce the probability of
+    // getting a 403 ('forbidden', i.e. server understood the request but refused to
+    // process it)
+    const headers = { 'User-Agent': USER_AGENT };
+
+    const config = { headers };
+    const response = await axios.get(url, config);
+
+    return response;
+}
 
 /**
  * @summary Fetches the basic info from {@link HOME_URL}
