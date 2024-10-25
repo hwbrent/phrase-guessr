0cf18fc45847eb9e1648c0f0f9a269c71b0b0c5b
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-10-26 00:21:15 +0100

    Add empty function

---
+++ src/lib/scrape/utils.js
@@ -0,0 +1,9 @@
+
+const HOME_URL = 'https://www.omniglot.com/soundfiles/';
+
+/**
+ * @summary Fetches the basic info from {@link HOME_URL}
+ * @description Basically just the languages and the urls of the three
+ * resources available for each one
+ */
+function getBascInfo() {}
