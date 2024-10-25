f250969ba6619b9033d0c7784976c6d38c80e2fa
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-10-26 00:56:53 +0100

    Get home page html document

--- src/lib/scrape/languages.js
+++ src/lib/scrape/languages.js
@@ -1,3 +1,21 @@
+import { JSDOM } from 'jsdom';
+
 import { HOME_URL, get } from './utils.js';
 
+async function getHomePage() {
+    const resp = await get(HOME_URL);
+    const html = resp.data;
 
+    const dom = new JSDOM(html);
+    const document = dom.window.document;
+
+    return document;
+}
+
+async function main() {
+    const document = await getHomePage();
+
+    console.log(document);
+}
+
+main()
