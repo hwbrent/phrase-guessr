dbe26a0d6717a478ffc0e795de6ca179f57fe088
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-10-26 19:18:36 +0100

    Fix error by adding getStaticPaths

--- src/pages/date/[date].tsx
+++ src/pages/date/[date].tsx
@@ -16,6 +16,25 @@ export async function getStaticProps() {
     return { props: { map, languages, resources } }
 }
 
+/**
+ * @summary Indicates that nothing needs to be created at build time
+ * @description I didn't think I needed this, since the static props don't depend on
+ * a dynamic query param or w/e, but when I ran the app, I got an error complaining
+ * about the lack of `getStaticPaths`:
+ *
+ *  Error: getStaticPaths is required for dynamic SSG pages and is missing for '/date/[date]'.
+ *  Read more: https://nextjs.org/docs/messages/invalid-getstaticpaths-value
+ *
+ * So I googled and found out that you can just call it with dummy values to indicate
+ * nothing needs to be done
+ * @returns Data indicating that nothing needs to be created at build time
+ * @see {@link https://stackoverflow.com/a/65783200}
+ */
+export const getStaticPaths = async () =>  ({
+    paths: [], // indicates that no page needs be created at build time
+    fallback: 'blocking' //indicates the type of fallback
+});
+
 export default function DatePage() {
     const router = useRouter();
     const { date } = router.query;
