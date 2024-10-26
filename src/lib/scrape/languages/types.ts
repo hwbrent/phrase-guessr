80acabf8215d8c1a1e8cec1b7ca915672389a053
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-10-26 10:42:36 +0100

    Remove individual url types

--- src/lib/scrape/languages/types.ts
+++ src/lib/scrape/languages/types.ts
@@ -24,34 +24,27 @@ export interface Language {
 }
 
 /**
- * A URL to a zip file containing mp3 recordings of a language
- * 
- * @example 'https://www.omniglot.com/soundfiles/gujarati/gujaratiphrases.zip'
- */
-export type RecordingsResourceURL = URL['href'];
-
-/**
- * A URL to a page containing a table of "Useful <language> phrases"
- * 
- * @example 'https://www.omniglot.com/language/phrases/kannada.php'
- */
-export type PhrasesResourceURL = URL['href'];
-
-/**
- * A URL to a page containing background info on the language
- * 
- * @example 'https://www.omniglot.com/writing/manx.htm'
- */
-export type LanguageResourceURL = URL['href'];
-
-/**
  * An object containing URLs pointing to a given language's further resources within
  * {@link https://www.omniglot.com}
  */
 export interface Resource {
-    recordings: RecordingsResourceURL;
-    phrases:    PhrasesResourceURL;
-    language:   LanguageResourceURL;
+    /**
+     * A URL to a zip file containing mp3 recordings of a language
+     * @example 'https://www.omniglot.com/soundfiles/gujarati/gujaratiphrases.zip'
+     */
+    recordings: URL['href'];
+
+    /**
+     * A URL to a page containing a table of "Useful <language> phrases"
+     * @example 'https://www.omniglot.com/language/phrases/kannada.php'
+     */
+    phrases: URL['href'];
+
+    /**
+     * A URL to a page containing background info on the language
+     * @example 'https://www.omniglot.com/writing/manx.htm'
+     */
+    language: URL['href'];
 }
 
 export type LanguageToResourceMap = Map<Language, Resource>;
