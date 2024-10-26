bcc16efadf1d2543ca51db71c1ae08fb9795539b
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-10-26 14:04:37 +0100

    Get random number for today

--- src/pages/api/random.ts
+++ src/pages/api/random.ts
@@ -11,6 +11,29 @@ function getTodayMidnight() {
     return midnight;
 }
 
+/**
+ * @summary Pseudo-random number generator based on the Linear Congruential Generator
+ * algorithm
+ * @see {@link https://en.wikipedia.org/wiki/Linear_congruential_generator}
+ */
+function getRandomNumber(seed: number): number {
+    const a = 1664525;
+    const c = 1013904223;
+    const m = Math.pow(2, 32);
+    seed = (a * seed + c) % m;
+    return seed / m;
+}
+
+/**
+ * @returns A random number between 0 and 1. Today's date is used as the seed, so no
+ * matter when you call this function on a given day, the output will be the same
+ */
+function getRandomNumberForToday() {
+    const seed = getTodayMidnight().valueOf();
+    const randomNumber = getRandomNumber(seed);
+    return randomNumber;
+}
+
 type ResponseData = {
   message: string
 }
@@ -19,5 +42,7 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  res.status(200).json({ message: `${getTodayMidnight()}` })
+    const randomNumber = getRandomNumberForToday();
+
+    res.status(200).json({ message: `${randomNumber}` })
 }
