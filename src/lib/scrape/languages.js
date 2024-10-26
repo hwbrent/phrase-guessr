1563a1c7ad15722a347d17080e833b19464102f4
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-10-26 01:07:59 +0100

    Add function to get main list on home page

--- src/lib/scrape/languages.js
+++ src/lib/scrape/languages.js
@@ -2,6 +2,14 @@ import { JSDOM } from 'jsdom';
 
 import { HOME_URL, get } from './utils.js';
 
+/**
+ * @summary A `Document` for the page at {@link HOME_URL}
+ * @typedef {Document} HomeDocument
+ */
+
+/**
+ * @returns {HomeDocument}
+ */
 async function getHomePage() {
     const resp = await get(HOME_URL);
     const html = resp.data;
@@ -12,10 +20,23 @@ async function getHomePage() {
     return document;
 }
 
+/**
+ * @param {HomeDocument} document
+ * @returns {HTMLUListElement} the main list on the page that we care about. it's the
+ * second <ul> on the page, and contains the language info (as well as hrefs to other
+ * important resources)
+ */
+function getMainList(document) {
+    const uls = document.getElementsByTagName('ul');
+    return uls[1];
+}
+
 async function main() {
     const document = await getHomePage();
 
-    console.log(document);
+    const mainList = getMainList(document);
+
+    console.log(mainList.outerHTML);
 }
 
 main()
