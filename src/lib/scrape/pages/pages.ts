62c5986cde6b8a4f1773d7e18fe7cb2ee0fc511f
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-10-26 11:24:04 +0100

    Create generic function getPage

--- src/lib/scrape/pages/pages.ts
+++ src/lib/scrape/pages/pages.ts
@@ -1,17 +1,19 @@
 import { JSDOM } from 'jsdom';
-
 import { get } from "../utils/utils";
-
 import { HomeDocument } from './types';
 
 export const HOME_URL = 'https://www.omniglot.com/soundfiles/';
 
-export async function getHomePage(): Promise<HomeDocument> {
-    const resp = await get(HOME_URL);
+async function getPage(url: string): Promise<Document> {
+    const resp = await get(url);
     const html = resp.data;
 
     const dom = new JSDOM(html);
     const document = dom.window.document;
 
     return document;
+}
+
+export async function getHomePage(): Promise<HomeDocument> {
+    return await getPage(HOME_URL);
 }
